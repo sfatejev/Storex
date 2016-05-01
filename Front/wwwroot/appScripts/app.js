07ae670664c9c518ba08070b3ef42322037b50e7
Sergei <ga3ttpom@gmail.com>
2016-05-01 21:57:26 +0300

    Fixes

--- Front/wwwroot/appScripts/app.js
+++ Front/wwwroot/appScripts/app.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: 'My First Angular 2 App'
+            template: 'AngularStorexApp'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
